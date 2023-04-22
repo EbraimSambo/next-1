294266d06742a7f343011e6c9e2cb7cd8c91377e
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2023-04-22 13:34:03 +0000

    Pending changes exported from your codespace

--- events-app/src/components/events/singleEvents.js
+++ events-app/src/components/events/singleEvents.js
@@ -2,13 +2,17 @@ import React from "react";
 import Image from "next/image";
 
 const SingleEvents  = ({data}) =>{
-
+     const onSubmit =() =>{}
     return(
         <div className="Single">  
          <h2> {data.title} </h2>
          <Image alt={data.title} src={data.image} width={500} height={400} />
          <p> {data.description} </p>
-         <input type='email' placeholder='Insira seu email' /> <button> Enviar</button>
+         <form onSubmit={onSubmit}>
+            <label for="">Registrar email</label>
+          <input type='email' placeholder='Insira seu email' /> <button type="button"> Enviar</button>
+         </form>
+       
         </div>
  )
 }
